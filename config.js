359c9f550525f3b2626b7684f1b068bd0e8639b6
naneer <reenan.arbitrario@gmail.com>
2015-04-02 05:34:39 +0000

    more setup

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ System.config({
 
 System.config({
   "map": {
+    "angular": "github:angular/bower-angular@1.3.15",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
   }
