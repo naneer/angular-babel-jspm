d6e83bacad17a97cf40a197a3475bc6c0119bbf2
naneer <reenan.arbitrario@gmail.com>
2015-04-03 20:13:24 +0000

    update to gulpfile to copy dep

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var gulp = require('gulp');
 var inject = require('gulp-inject');
  
 gulp.task('index', function () {
+  
+  gulp.src(['./src/*.js'])
+    .pipe(gulp.dest('./dist'));
+
+  gulp.src(['./src/jspm_packages/*.js', './src/jspm_packages/*.map'])
+    .pipe(gulp.dest('./dist/jspm_packages'));
+  
   var target = gulp.src('./src/index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths: 
   var sources = gulp.src(['build.js'], {read: false, cwd: './dist'});
- 
+  
   return target.pipe(inject(sources, {addRootSlash: false}))
     .pipe(gulp.dest('./dist'));
 });
