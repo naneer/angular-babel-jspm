c852aa198487845f1ea05fb3980706b8477f4d7a
naneer <reenan.arbitrario@gmail.com>
2015-04-08 07:05:08 +0000

    fix protractor test

--- src/lib/routes/home/home.e2e.js
+++ src/lib/routes/home/home.e2e.js
@@ -1,6 +1,6 @@
 describe('home', function() {
   it('has ui-view of index', function() {
     browser.get('/');
-    expect(element(by.css('ui-view')).getText()).toEqual('index');
+    expect(element(by.css('div[ui-view="index"]')).toBeDefined());
   });
 });
