49d7cdd2a7f24b0378b3c9375cb97bc8be41e3f6
naneer <reenan.arbitrario@gmail.com>
2015-04-10 18:37:03 +0000

    ui changes for validations

--- src/lib/routes/join/join.controller.js
+++ src/lib/routes/join/join.controller.js
@@ -6,36 +6,46 @@ import {authModule} from 'lib/services/auth/auth';
 
 const AUTH = new WeakMap();
 const STATE = new WeakMap();
+const Q = new WeakMap();
+
 class joinRouteController{
-  constructor(Auth, $state){
+  constructor(Auth, $state, $q){
     AUTH.set(this, Auth);
     STATE.set(this, $state);
+    Q.set(this, $q);
+    this.loading = false;
   }
+  
   join(){
-    if(!this.form.$valid){
-      console.log(this.form);
-      return console.log('Please correct non-valid input');
-    }
-    AUTH.get(this).$createUser({
-      email: this.email,
-      password: this.password
+    var ctrl = this;
+    var email = ctrl.email,
+        password = ctrl.password,
+        auth = AUTH.get(ctrl);
+    
+    if (!ctrl.form.$valid) return;
+    
+    ctrl.loading = true;
+    
+    auth.$createUser({
+      email: email,
+      password: password
     }).then(function(userData){
         console.log("User created successfully", userData);
-        AUTH.get(this).$authWithPassword({
-          email: this.email,
-          password: this.password
-        }).then(function(authData){
-          console.log("logged in", authData);
-        }).catch(function(error){
-          console.log("err", error);
+        return auth.$authWithPassword({
+          email: email,
+          password: password
         });
+    }).then(function(authData){
+        console.log("User is logged in", authData);
     }).catch(function(error){
         console.log("Error Occurred", error);
+    }).then(function(){
+        ctrl.loading = false;
     });
   }
 }
 
-joinRouteController.$inject = ['Auth', '$state'];
+joinRouteController.$inject = ['Auth', '$state', '$q'];
 
 export var joinRouteControllerModule = angular.module('joinRouteControllerModule', [
   'ui.router',
